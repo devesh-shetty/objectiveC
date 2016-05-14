09709d962bf90d2de4818c45b40e66dffb22169c
Devesh Shetty <devesh.shetty007@gmail.com>
2016-05-14 15:26:26 +0530

    Initial Commit

---
+++ dContacts/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  dContacts
+//
+//  Created by Devesh Shetty on 14/05/16.
+//  Copyright © 2016 Devesh Shetty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ dContacts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  dContacts
+//
+//  Created by Devesh Shetty on 14/05/16.
+//  Copyright © 2016 Devesh Shetty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
