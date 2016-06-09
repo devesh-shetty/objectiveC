f0ca63380daa6b1e387ed56e44caddc4675a9b61
Devesh Shetty <devesh.shetty007@gmail.com>
2016-06-10 04:45:54 +0530

    Exploring objective c

---
+++ ExploringObjectiveC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ExploringObjectiveC
+//
+//  Created by Devesh Shetty on 10/06/16.
+//  Copyright © 2016 Devesh Shetty. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
