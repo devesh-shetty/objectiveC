0e0493422d39b3d085df3510dd8535c996a90bac
Devesh Shetty <devesh.shetty007@gmail.com>
2016-06-10 17:10:44 +0530

    Tap me working with timer

--- Tap Me/Tap Me/ViewController.m
+++ Tap Me/Tap Me/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self setUpGame];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,8 +25,37 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(void)setUpGame{
+    
+    seconds = 30;
+    count = 0;
+    
+    timerLabel.text = [NSString stringWithFormat:@"Time: %li",seconds];
+    scoreLabel.text = [NSString stringWithFormat:@"Score \n%li",count];
+    
+    //param 1 the time interval
+    //target is which instance to send a message to every second. Here, we want the message to go to the View Controller hence the target is self
+    //selector is what method we want to call. We specify the method using @selector();
+    //userInfo is any extra info you want stored with the timer
+    //repeat = YES because we want the timer to repeat itself
+    timer = [NSTimer scheduledTimerWithTimeInterval:1.0f target:self selector:@selector(subtractTime) userInfo:nil repeats:YES];
+    
+}
+
+-(void)subtractTime{
+    seconds--;
+    timerLabel.text = [NSString stringWithFormat:@"Time: %li",seconds];
+    
+    if(seconds == 0){
+        //stop the timer
+        [timer invalidate];
+    }
+    
+}
+
 -(IBAction)buttonPressed{
-    NSLog(@"Button Pressed");
+    count++;
+    scoreLabel.text = [NSString stringWithFormat:@"Score \n%li",count];
 }
 
 @end
